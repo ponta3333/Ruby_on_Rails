a6d7b7c54b2bc25ac1d020f0d4d46c541f522b66
$ ruby -v <ruby -v>
2020-07-21 16:50:06 +0900

    [Finish]4-7

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -18,6 +18,19 @@ class BlogsController < ApplicationController
   end
 
   def edit
+  	@blog = Blog.find(params[:id])
+  end
+
+  def update
+  	blog = Blog.find(params[:id])
+  	blog.update(blog_params)
+  	redirect_to blog_path(blog)
+  end
+
+  def destroy
+  	blog = Blog.find(params[:id])
+  	blog.destroy
+  	redirect_to blogs_path
   end
 
   private
