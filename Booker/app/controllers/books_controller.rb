52e4d19527da3da967a671bb511abe871dc1a18c
$ ruby -v <ruby -v>
2020-07-23 03:04:42 +0900

    [Finish]4-8

--- Booker/app/controllers/books_controller.rb
+++ Booker/app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
   	@book = Book.new(book_params)
   	@books = Book.all
   	if @book.save
-  		flash[:notice] = "Book was succesfully created."
+  		flash[:notice] = "Book was successfully created."
   		redirect_to book_path(@book.id)
   	else
   		render :index
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
   def update
   	@book = Book.find(params[:id])
   	if @book.update(book_params)
-  		flash[:notice] = "Book was succesfully created."
+  		flash[:notice] = "Book was successfully updated."
   		redirect_to book_path(@book.id)
   	else
   		render :edit
