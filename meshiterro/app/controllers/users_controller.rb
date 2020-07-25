c659d50230be928b4767f5437f5c4ac991b5ff42
$ ruby -v <i.i.i.you.vvv.333@gmail.com>
2020-07-25 22:16:27 +0900

    [Start]5-6

--- meshiterro/app/controllers/users_controller.rb
+++ meshiterro/app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+
   def show
   	@user = User.find(params[:id])
   	@post_images = @user.post_images.page(params[:page]).reverse_order
   end
+
+def edit
+	@user = user.find(params[:id])
+
 end
