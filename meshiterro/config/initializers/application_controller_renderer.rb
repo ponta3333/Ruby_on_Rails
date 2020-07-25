ead79a4358658cf40489ef5eb5fb14b5d0078190
$ ruby -v <ruby -v>
2020-07-25 21:52:23 +0900

    [Finish]5-6

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '358cb08b138d894d32024fe3f868cf06c241fd9e23472a6415fccc1299b6eae9fa236fa607949beea000613752e23e2e6841365700b0cb03b8428766478b8924'
---
+++ meshiterro/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  	resources :favorites, only: [:create, :destroy]
+  	resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show]
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
