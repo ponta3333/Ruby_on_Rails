1fc228f97981278bf88a316e9f15cee87f77a170
$ ruby -v <i.i.i.you.vvv.333@gmail.com>
2020-08-15 23:00:47 +0900

    [Finish]iine

--- meshiterro_re/config/routes.rb
+++ meshiterro_re/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'post_images#index'
   resources :post_images, only: [:new, :create, :index, :show] do
+  	resources :favorites, only: [:create, :destroy]
   	resources :post_comments, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- meshiterro_re/db/schema.rb
+++ meshiterro_re/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_094031) do
+ActiveRecord::Schema.define(version: 2020_08_15_130705) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_comments", force: :cascade do |t|
     t.text "content"
